988ceac592af8bebff9b176d540475f58ca45455
HDeBoever <henrideboever@gmail.com>
2020-09-11 12:04:08 -0700

    added DB functionality

--- django_project/blog/models.py
+++ django_project/blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your database models here. Tables for users, but posts also.
+# Django has a built in authentication system, as well as user model.
+
+# Each class is its own table in the database
+
+# Creating the model for posts
+class Post(models.Model):
+    # title field is a character field with a restriction of 100 characters in length
+    title = models.CharField(max_length = 100)
+
+    # Unrestricted in length
+    content = models.TextField()
+
+    # Date is the date of the post when the post was created
+    date_posted = models.DateTimeField(default = timezone.now)
+
+    # Author field. Users author posts. 1 to many relationship. If a user is deleted, also delete their post.
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,25 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-posts = [
-    {
-        'author' : 'Henri',
-        'title' : 'blog post 1',
-        'content' : 'First post contents',
-        'date_posted' : '2020/9/11'
-    },
-    {
-        'author' : 'Amanda',
-        'title' : 'blog post 2',
-        'content' : 'Second post contents',
-        'date_posted' : '2020/9/10'
-    }
-]
+from .models import Post
 
 # Home page route, returns a rendered template
 def home(request):
 
-    context = {'posts' : posts}
+    # Uses the posts from the post database
+    context = {'posts' : Post.objects.all()}
     return render(request, 'blog/home.html', context)
 
 # About page route, returns a rendered template
